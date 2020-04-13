10895e9bd3fb8f576cdf7168e4793e48659dc1af
Ryad Benadjila <ryadbenadjila@gmail.com>
2020-04-13 09:05:22 +0200

    Add default word size for SH4 and SPARC architectures.

--- src/words/words.h
+++ src/words/words.h
@@ -54,7 +54,8 @@
  * option (see below)!
  */
 #if defined(__x86_64__) || defined(__i386__) || defined(__ppc64__) || defined(__ppc__) ||\
-    defined(__arm__) || defined(__aarch64__) || defined(__mips__) || defined(__s390x__)
+    defined(__arm__) || defined(__aarch64__) || defined(__mips__) || defined(__s390x__) ||\
+    defined(__SH4__) || defined(__sparc__)
 #define WORDSIZE 64
 #include "words_64.h"
 #else
