4be9d9b7bdb6a5ed3d4ba40800cdb2e846eefe55
Arnaud EBALARD <arnaud.ebalard@ssi.gouv.fr>
2021-09-23 23:07:01 +0200

    return/eddsa: make functions return int for unexpected error instead of performing while(1) loop
    Signed-off-by: Arnaud EBALARD <arnaud.ebalard@ssi.gouv.fr>
    Signed-off-by: Ryad Benadjila <ryadbenadjila@gmail.com>

--- src/sig/eddsa.h
+++ src/sig/eddsa.h
@@ -53,7 +53,7 @@ struct ec_sign_context;
 int eddsa_gen_priv_key(ec_priv_key *priv_key);
 int eddsa_init_pub_key(ec_pub_key *out_pub, const ec_priv_key *in_priv);
 
-u8 eddsa_siglen(u16 p_bit_len, u16 q_bit_len, u8 hsize, u8 blocksize);
+int eddsa_siglen(u16 p_bit_len, u16 q_bit_len, u8 hsize, u8 blocksize, u8 *siglen);
 
 int _eddsa_sign_init_pre_hash(struct ec_sign_context *ctx);
 
