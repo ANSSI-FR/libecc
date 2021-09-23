b0b64e8bd63ae3e524cbe13aaec02e8453801ae2
Ryad Benadjila <ryadbenadjila@gmail.com>
2021-09-23 23:07:01 +0200

    return/sha3: make functions return int for unexpected error instead of performing while(1) loop
    Signed-off-by: Ryad Benadjila <ryadbenadjila@gmail.com>
    Signed-off-by: Arnaud EBALARD <arnaud.ebalard@ssi.gouv.fr>

--- src/hash/sha3.c
+++ src/hash/sha3.c
@@ -16,82 +16,93 @@
 #include "../utils/utils.h"
 #include "sha3.h"
 
-/* Init function depending on the digest size */
-void _sha3_init(sha3_context *ctx, u8 digest_size)
+/* Init function depending on the digest size. Return 0 on success, -1 on error. */
+int _sha3_init(sha3_context *ctx, u8 digest_size)
 {
-	/* Check given inpur digest size: we only consider KECCAK versions 
+	int ret;
+
+	/*
+	 * Check given inpur digest size: we only consider KECCAK versions
 	 * mapped on SHA-3 instances (224, 256, 384, 512).
 	 */
-        MUST_HAVE( (digest_size == (224/8)) || (digest_size == (256/8)) || \
-                (digest_size == (384/8)) || (digest_size == (512/8)) );
+	MUST_HAVE(((digest_size == (224/8)) || (digest_size == (256/8)) ||
+		   (digest_size == (384/8)) || (digest_size == (512/8))), ret, err);
+	MUST_HAVE((ctx != NULL), ret, err);
 
-	MUST_HAVE(ctx != NULL);
+	/* Zeroize the internal state */
+	local_memset(ctx->sha3_state, 0, sizeof(ctx->sha3_state));
 
-        /* Zeroize the internal state */
-        local_memset(ctx->sha3_state, 0, sizeof(ctx->sha3_state));
-
-        ctx->sha3_idx = 0;
-        ctx->sha3_digest_size = digest_size;
-        ctx->sha3_block_size = (KECCAK_SLICES * KECCAK_SLICES * sizeof(u64)) - (2 * digest_size);
+	ctx->sha3_idx = 0;
+	ctx->sha3_digest_size = digest_size;
+	ctx->sha3_block_size = (KECCAK_SLICES * KECCAK_SLICES * sizeof(u64)) - (2 * digest_size);
 
 	/* Detect endianness */
 	ctx->sha3_endian = arch_is_big_endian() ? SHA3_BIG : SHA3_LITTLE;
 
-        return;
+	ret = 0;
+
+err:
+	return ret;
 }
 
-/* Update hash function */
-void _sha3_update(sha3_context *ctx, const u8 *input, u32 ilen)
+/* Update hash function. Returns 0 on sucess, -1 on error. */
+int _sha3_update(sha3_context *ctx, const u8 *input, u32 ilen)
 {
-        u32 i;
-        u8 *state;
+	u32 i;
+	u8 *state;
+	int ret;
 
-	MUST_HAVE((ctx != NULL) && (input != NULL));
+	MUST_HAVE(((ctx != NULL) && (input != NULL)), ret, err);
 
-        state = (u8*)(ctx->sha3_state);
+	state = (u8*)(ctx->sha3_state);
 
-        for(i = 0; i < ilen; i++){
-                /* Compute the index depending on the endianness */
-                u64 idx = (ctx->sha3_endian == SHA3_LITTLE) ? ctx->sha3_idx : SWAP64_Idx(ctx->sha3_idx);
-                ctx->sha3_idx++;
-                /* Update the state, and adapt endianness order */
-                state[idx] ^= input[i];
-                if(ctx->sha3_idx == ctx->sha3_block_size){
-                        KECCAKF(ctx->sha3_state);
-                        ctx->sha3_idx = 0;
-                }
-        }
+	for (i = 0; i < ilen; i++) {
+		u64 idx = (ctx->sha3_endian == SHA3_LITTLE) ? ctx->sha3_idx : SWAP64_Idx(ctx->sha3_idx);
+		ctx->sha3_idx++;
+		/* Update the state, and adapt endianness order */
+		state[idx] ^= input[i];
+		if(ctx->sha3_idx == ctx->sha3_block_size){
+			KECCAKF(ctx->sha3_state);
+			ctx->sha3_idx = 0;
+		}
+	}
 
-        return;
+	ret = 0;
+
+err:
+	return ret;
 }
 
-/* Finalize hash function */
-void _sha3_finalize(sha3_context *ctx, u8 *output)
+/* Finalize hash function. Returns 0 on success, -1 on error. */
+int _sha3_finalize(sha3_context *ctx, u8 *output)
 {
-        unsigned int i;
-        u8 *state;
+	unsigned int i;
+	u8 *state;
+	int ret;
 
-	MUST_HAVE((ctx != NULL) && (output != NULL));
-	MUST_HAVE(ctx->sha3_digest_size <= sizeof(ctx->sha3_state));
+	MUST_HAVE((output != NULL), ret, err);
+	MUST_HAVE(ctx->sha3_digest_size <= sizeof(ctx->sha3_state), ret, err);
 
-        state = (u8*)(ctx->sha3_state);
+	state = (u8*)(ctx->sha3_state);
 
-        /* Proceed with the padding of the last block */
-        /* Compute the index depending on the endianness */
-        if(ctx->sha3_endian == SHA3_LITTLE){
-                /* Little endian case */
-                state[ctx->sha3_idx] ^= 0x06;
-                state[ctx->sha3_block_size - 1] ^= 0x80;
-        }
-        else{
-                /* Big endian case */
-                state[SWAP64_Idx(ctx->sha3_idx)] ^= 0x06;
-                state[SWAP64_Idx(ctx->sha3_block_size - 1)] ^= 0x80;
-        }
-        KECCAKF(ctx->sha3_state);
-        for(i = 0; i < ctx->sha3_digest_size; i++){
-                output[i] = (ctx->sha3_endian == SHA3_LITTLE) ? state[i] : state[SWAP64_Idx(i)];
-        }
+	/* Proceed with the padding of the last block */
+	/* Compute the index depending on the endianness */
+	if (ctx->sha3_endian == SHA3_LITTLE) {
+		/* Little endian case */
+		state[ctx->sha3_idx] ^= 0x06;
+		state[ctx->sha3_block_size - 1] ^= 0x80;
+	} else {
+		/* Big endian case */
+		state[SWAP64_Idx(ctx->sha3_idx)] ^= 0x06;
+		state[SWAP64_Idx(ctx->sha3_block_size - 1)] ^= 0x80;
+	}
+	KECCAKF(ctx->sha3_state);
+	for(i = 0; i < ctx->sha3_digest_size; i++){
+		output[i] = (ctx->sha3_endian == SHA3_LITTLE) ? state[i] : state[SWAP64_Idx(i)];
+	}
 
-        return;
+	ret = 0;
+
+err:
+	return ret;
 }
