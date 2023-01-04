b4f6e6aa52251cf47cadb81112cdf4ec6e01911a
Ryad Benadjila <ryadbenadjila@gmail.com>
2023-01-04 15:08:54 +0100

    Small fix in the expand script.

--- scripts/expand_libecc.py
+++ scripts/expand_libecc.py
@@ -218,7 +218,7 @@ class Point(object):
             raise Exception("Point add error: two point don't have the same curve")
         # If Q is infinity point, return ourself
         if Q.x == None:
-            return Point(self.curve, None, None)
+            return Point(self.curve, self.x, self.y)
         # If we are the infinity point return Q
         if self.x == None:
             return Q
@@ -262,6 +262,11 @@ class Point(object):
     # Deep copy is implemented using the ~X operator
     def __invert__(self):
         return copy.deepcopy(self)
+    def __str__(self):
+        if self.x == None:
+            return "Inf"
+        else:
+            return ("(x = %s, y = %s)" % (hex(self.x), hex(self.y)))
 
 ##########################################################
 ### Private and public keys structures
